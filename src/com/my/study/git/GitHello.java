4b14b190c8e1cd135d498fbbc5a9cc388a6ab69b
45000 <45000232@qq.com>
2020-07-03 17:47:48 +0800

    添加工程到git

---
+++ src/com/my/study/git/GitHello.java
@@ -0,0 +1,11 @@
+package com.my.study.git;
+
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
